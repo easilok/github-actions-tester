c551b739dc98f55f2f64a647596be12f437b13f7
Luis Pereira <luispereira.tkd@gmail.com>
2024-12-27 17:06:37 +0000

    package: chuck norris package to test cache

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 
 console.log("Hello")
 console.log("World!")
+var chucknorrisAPI = require('@apiverve/chucknorris');
+var api = new chucknorrisAPI({
+    api_key: "123",
+    secure: true //(Optional, defaults to true)
+});
+
+
+
+api.execute(function (error, data) {
+    if (error) {
+        return console.error(error);
+    } else {
+        console.log(data);
+    }
+});
+
